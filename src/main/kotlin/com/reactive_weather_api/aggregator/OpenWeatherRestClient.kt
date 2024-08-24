e9d58b428c5264380a19453adf1b2440e97c3d6b
sshehrozali <sshehroza13@gmail.com>
2024-08-24 21:45:53 +0500

    Optimised imports

--- src/main/kotlin/com/reactive_weather_api/aggregator/OpenWeatherRestClient.kt
+++ src/main/kotlin/com/reactive_weather_api/aggregator/OpenWeatherRestClient.kt
@@ -1,21 +1,16 @@
 package com.reactive_weather_api.aggregator
 
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.module.kotlin.convertValue
 import com.reactive_weather_api.aggregator.exception.OpenWeatherRestClientException
 import com.reactive_weather_api.aggregator.model.DirectGeocodingData
 import com.reactive_weather_api.aggregator.model.GetDirectGeocodingResponseDTO
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.http.HttpStatus
 import org.springframework.http.HttpStatusCode
 import org.springframework.http.MediaType
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClient
-import org.springframework.web.reactive.function.client.awaitBody
 import org.springframework.web.reactive.function.client.bodyToMono
 import reactor.core.publisher.Mono
-import java.util.logging.Logger
 
 @Component
 class OpenWeatherRestClient(
