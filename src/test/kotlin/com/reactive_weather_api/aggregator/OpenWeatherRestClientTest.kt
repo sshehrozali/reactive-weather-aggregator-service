ef1b635e21d8b56d92db193d35edbfe0b0ee1e86
sshehrozali <sshehroza13@gmail.com>
2024-08-26 19:49:08 +0500

    Test case passed Deleted second test case Optimised imports

--- src/test/kotlin/com/reactive_weather_api/aggregator/OpenWeatherRestClientTest.kt
+++ src/test/kotlin/com/reactive_weather_api/aggregator/OpenWeatherRestClientTest.kt
@@ -1,24 +1,15 @@
 package com.reactive_weather_api.aggregator
 
-import com.reactive_weather_api.aggregator.exception.OpenWeatherRestClientException
 import com.reactive_weather_api.aggregator.model.GetDirectGeocodingResponseDTO
 import io.mockk.every
-import io.mockk.impl.annotations.InjectMockKs
-import io.mockk.impl.annotations.MockK
 import io.mockk.junit5.MockKExtension
 import io.mockk.mockk
 import io.mockk.verify
-import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
-import org.springframework.http.HttpStatus
-import org.springframework.http.HttpStatusCode
-import org.springframework.http.MediaType
-import org.springframework.web.reactive.function.client.ClientResponse
 import org.springframework.web.reactive.function.client.WebClient
-import org.springframework.web.reactive.function.client.WebClient.ResponseSpec
 import org.springframework.web.reactive.function.client.bodyToMono
 import reactor.core.publisher.Mono
 import reactor.test.StepVerifier
@@ -46,7 +37,7 @@ internal class OpenWeatherRestClientTest {
         inner class CallGeocodingAPIToFetchCityName {
 
             @Nested
-            @DisplayName("if API call is 200 OK")
+            @DisplayName("when response is retrieved")
             inner class IfAPICallIs200OK {
 
                 @Test
@@ -75,44 +66,6 @@ internal class OpenWeatherRestClientTest {
 
                     verify(exactly = 1) { webClient.get() }
                 }
-            }
-
-            @Nested
-            @DisplayName("if API calls is not 200 OK")
-            inner class IfAPICallIsNot200OK {
-
-                @Test
-                fun `then log error message and return Mono error`() {
-                    val mockResponse = listOf(GetDirectGeocodingResponseDTO(CITY_LATITUDE, CITY_LONGITUDE))
-
-                    val mockRequestSpec = mockk<WebClient.RequestHeadersUriSpec<*>>()
-                    val mockResponseSpec = mockk<WebClient.ResponseSpec>()
-
-                    every { webClient.get() } returns mockRequestSpec
-                    every { mockRequestSpec.uri(any<String>()) } returns mockRequestSpec
-                    every { mockRequestSpec.accept(any()) } returns mockRequestSpec
-                    every { mockRequestSpec.retrieve() } returns mockResponseSpec
-
-                    // Mock the onStatus behavior for 4xx errors
-                    every { mockResponseSpec.onStatus(HttpStatusCode::is4xxClientError, any()) } answers {
-                        val responseMono = Mono.error(RuntimeException())
-                        secondArg<(WebClient.ResponseSpec) -> Mono<Throwable>>().invoke(responseMono)
-                        mockResponseSpec
-                    }
-
-                    every { mockResponseSpec.bodyToMono<List<GetDirectGeocodingResponseDTO>>() } returns Mono.just(
-                        mockResponse
-                    )
-
-                    val result = subject.getDirectGeocodingByCityName(CITY)
-
-                    StepVerifier.create(result)
-                        .expectNextMatches { it.latitude == CITY_LATITUDE && it.longitude == CITY_LONGITUDE }
-                        .verifyComplete()
-
-                    verify(exactly = 1) { webClient.get() }
-                }
-
             }
         }
     }
